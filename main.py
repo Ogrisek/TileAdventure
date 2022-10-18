6773111258866d1a382bc7e393751030ab84c226
Ogrisek <115953716+Ogrisek@users.noreply.github.com>
2022-10-18 22:13:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import copy
 
 def main():
     start_fenster()
-    #game_fenster_update()
+    game_fenster_update()
     window.mainloop()
 
 def rng(height, width):
@@ -34,30 +34,30 @@ def quit_button_creator():
 def handle_click_quit(event):
     window.quit()
 
+# noinspection PyGlobalUndefined
 def start_button_creator():
-    # noinspection PyGlobalUndefined
     global start_button
     start_button = tk.Button(text="Click me!", background="turquoise")
     start_button.bind("<Button-1>", handle_click_start)
     start_button.pack()
-    # noinspection PyGlobalUndefined
     global game_beatable
     game_beatable = 0
+    global tile_type_save
+    tile_type_save = 1
 
 def handle_click_start(event):
     start_button.destroy()
     quit_button.destroy()
-    handle_click_play()
+    play()
 
-def handle_click_play():
-    # noinspection PyGlobalUndefined
+# noinspection PyGlobalUndefined
+def play():
     global game_beatable
-    print(game_beatable)
+    game_beatable = 0
     while game_beatable == 0:
         tile_type_creator()
         check_game_beatable()
     game_frame_creator()
-
 
 def game_fenster():
     greeting = tk.Label(text="Daniel ist ein netter Mensch")
@@ -65,24 +65,137 @@ def game_fenster():
 
 def game_frame_creator():
     window.geometry("512x512")
-    screen_height_setting = 5
-    screen_width_setting = 5
+    #screen_height_setting = 5
+    #screen_width_setting = 5
 
-    for screen_height in range(screen_height_setting):
+    for screen_height in range(map_height + 3):
         window.rowconfigure(screen_height, minsize=64)
-        for screen_width in range(screen_width_setting):
+        for screen_width in range(map_width):
             window.columnconfigure(screen_width, minsize=64)
             # noinspection PyGlobalUndefined
-            global frame_game
+            #global frame_game
             frame_game = tk.Frame(master=window)
             frame_game.grid(row=screen_height, column=screen_width)
-            tile_label = tk.Label(master=frame_game, text=f"Row {screen_height}\nColumn {screen_width}")
-            tile_label.grid()
+            #tile_label = tk.Label(master=frame_game, text=f"Row {screen_height}\nColumn {screen_width}")
+            #tile_label.grid()
             tile_updater()
 
-def tile_updater():
-    #get color for tile typ
+    button_up = tk.Button(master=window, bg="beige", text="↑")
+    button_down = tk.Button(master=window, bg="beige", text="↓")
+    button_left = tk.Button(master=window, bg="beige", text="←")
+    button_right = tk.Button(master=window, bg="beige", text="→")
+    button_interact = tk.Button(master=window, bg="beige", text="use")
+    button_quit = tk.Button(master=window, bg="beige", text="Quit")
+    button_restart = tk.Button(master=window, bg="beige", text="Restart")
 
+    button_up.bind("<Button-1>", handle_click_up)
+    button_down.bind("<Button-1>", handle_click_down)
+    button_left.bind("<Button-1>", handle_click_left)
+    button_right.bind("<Button-1>", handle_click_right)
+    #button_interact.bind("<Button-1>", handle_click_interact)
+    button_quit.bind("<Button-1>", handle_click_quit)
+    button_restart.bind("<Button-1>", handle_click_start)
+
+    button_up.grid(row=5, column=1, sticky="nesw")
+    button_down.grid(row=7, column=1, sticky="nesw")
+    button_left.grid(row=6, column=0, sticky="nesw")
+    button_right.grid(row=6, column=2, sticky="nesw")
+    button_interact.grid(row=6, column=1, sticky="nesw")
+    button_quit.grid(row=7, column=4, sticky="nesw")
+    button_restart.grid(row=6, column=4, sticky="nesw")
+
+# noinspection PyGlobalUndefined
+def handle_click_up(event):
+    player_height_coordinate, player_width_coordinate = search_tile_state(2)
+    global tile_type_save
+    global matrix
+
+    if player_height_coordinate - 1 < 0:
+        print("walk error map border")
+    else:
+        if matrix[player_height_coordinate - 1][player_width_coordinate] == 0:
+            print("walk error tree")
+        else:
+            if matrix[player_height_coordinate - 1][player_width_coordinate] == 5:
+                print("dead")
+            else:
+                tile_type_save_new = matrix[player_height_coordinate - 1][player_width_coordinate]
+                matrix[player_height_coordinate - 1][player_width_coordinate] = 2
+                matrix[player_height_coordinate][player_width_coordinate] = tile_type_save
+                tile_type_save = tile_type_save_new
+
+    tile_updater()
+
+# noinspection PyGlobalUndefined
+def handle_click_down(event):
+    player_height_coordinate, player_width_coordinate = search_tile_state(2)
+    global tile_type_save
+    global matrix
+
+    if player_height_coordinate + 1 > map_height - 1:
+        print("walk error map border")
+    else:
+        if matrix[player_height_coordinate + 1][player_width_coordinate] == 0:
+            print("walk error tree")
+        else:
+            if matrix[player_height_coordinate + 1][player_width_coordinate] == 5:
+                print("dead")
+            else:
+                tile_type_save_new = matrix[player_height_coordinate + 1][player_width_coordinate]
+                matrix[player_height_coordinate + 1][player_width_coordinate] = 2
+                matrix[player_height_coordinate][player_width_coordinate] = tile_type_save
+                tile_type_save = tile_type_save_new
+
+    tile_updater()
+
+# noinspection PyGlobalUndefined
+def handle_click_left(event):
+    player_height_coordinate, player_width_coordinate = search_tile_state(2)
+    global tile_type_save
+    global matrix
+
+    if player_width_coordinate - 1 < 0:
+        print("walk error map border")
+    else:
+        if matrix[player_height_coordinate][player_width_coordinate - 1] == 0:
+            print("walk error tree")
+        else:
+            if matrix[player_height_coordinate][player_width_coordinate - 1] == 5:
+                print("dead")
+            else:
+                tile_type_save_new = matrix[player_height_coordinate][player_width_coordinate - 1]
+                matrix[player_height_coordinate][player_width_coordinate - 1] = 2
+                matrix[player_height_coordinate][player_width_coordinate] = tile_type_save
+                tile_type_save = tile_type_save_new
+
+    tile_updater()
+
+# noinspection PyGlobalUndefined
+def handle_click_right(event):
+    player_height_coordinate, player_width_coordinate = search_tile_state(2)
+    global tile_type_save
+    global matrix
+
+    if player_width_coordinate + 1 > map_width - 1:
+        print("walk error map border")
+    else:
+        if matrix[player_height_coordinate][player_width_coordinate + 1] == 0:
+            print("walk error tree")
+        else:
+            if matrix[player_height_coordinate][player_width_coordinate + 1] == 5:
+                print("dead")
+            else:
+                tile_type_save_new = matrix[player_height_coordinate][player_width_coordinate + 1]
+                matrix[player_height_coordinate][player_width_coordinate + 1] = 2
+                matrix[player_height_coordinate][player_width_coordinate] = tile_type_save
+                tile_type_save = tile_type_save_new
+
+    tile_updater()
+
+def game_fenster_update():
+    print("yes")
+
+def tile_updater():
     tile_type_0_0 = get_tile_type(0, 0)
     tile_type_0_1 = get_tile_type(0, 1)
     tile_type_0_2 = get_tile_type(0, 2)
@@ -205,15 +318,11 @@ def tile_type_creator():
     width_coordinates = width_coordinates - 1
     matrix[height_coordinates][width_coordinates] = 2
 
-    #count_tree = 0
     amount_tree = int((map_height*map_width)/3)
-    #print(amount_tree)
     for count_tree in range(amount_tree):
         height_coordinates, width_coordinates = rng(map_height, map_width)
         height_coordinates = height_coordinates - 1
         width_coordinates = width_coordinates - 1
-        #print(height_coordinates)
-        #print(width_coordinates)
 
         while matrix[height_coordinates][width_coordinates] != 1:
             amount_tree = amount_tree + 1
@@ -250,8 +359,6 @@ def tile_type_creator():
 # noinspection PyGlobalUndefined
 def check_game_beatable():
     global game_beatable
-    #game_beatable = 1
-    print(matrix)
     matrix_check = copy.deepcopy(matrix)
     for x in range(map_height):
         for y in range(map_width):
@@ -263,9 +370,7 @@ def check_game_beatable():
                 matrix_check[x][y] = 1
             if matrix_check[x][y] == 5:
                 matrix_check[x][y] = 0
-    print(matrix_check)
 
-    #funktioniert nicht richtig
     grid = Grid(matrix=matrix_check)
     finder = AStarFinder()
 
@@ -274,47 +379,30 @@ def check_game_beatable():
     key_height, key_width = search_tile_state(4)
     boss_height, boss_width = search_tile_state(5)
 
-    print(search_tile_state(2))
-    print(search_tile_state(4))
-
-    start = grid.node(start_height, start_width)
-    key = grid.node(key_height, key_width)
-    chest = grid.node(chest_height, chest_width)
-    boss = grid.node(boss_height, boss_width)
+    start = grid.node(start_width, start_height)
+    key = grid.node(key_width, key_height)
+    chest = grid.node(chest_width, chest_height)
 
     path, runs = finder.find_path(start, key, grid)
-    print(path)
     if path:
         Grid.cleanup(grid)
-        print(search_tile_state(4))
         path, runs = finder.find_path(key, chest, grid)
-        print("true")
-        print(path)
         if path:
             Grid.cleanup(grid)
-            i, j = search_tile_state(5)
-            matrix_check[i][j] = 1
-            grid = Grid(matrix=matrix_check)
-            print(matrix_check, "f")
-            path, runs = finder.find_path(chest, boss, grid)
-            print("true2")
-            print(path)
+            matrix_check[boss_height][boss_width] = 1
+            grid_boss = Grid(matrix=matrix_check)
+            chest = grid_boss.node(chest_width, chest_height)
+            boss = grid_boss.node(boss_width, boss_height)
+            path, runs = finder.find_path(chest, boss, grid_boss)
             if path:
-                # game_beatable = 1
-                print("true3")
+                game_beatable = 1
             else:
-                print("false3")
+                game_beatable = 0
         else:
-            print("false2")
+            game_beatable = 0
     else:
-        print("false")
-    game_beatable = 1
+        game_beatable = 0
     Grid.cleanup(grid)
-    #print(player_height, player_width)
-
-    #for player_height in range(map_height):
-    #    for player_width in range(map_width):
-
 
 # noinspection PyGlobalUndefined
 def search_tile_state(tile_value):
